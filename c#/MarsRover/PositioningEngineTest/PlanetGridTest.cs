eaf88b990f173e07add4e88ba27513e9eca55c14
nerolauda <nerolauda@gmail.com>
2021-06-06 17:57:23 +0200

    Removed useless tests

--- c#/MarsRover/PositioningEngineTest/PlanetGridTest.cs
+++ c#/MarsRover/PositioningEngineTest/PlanetGridTest.cs
@@ -42,61 +42,7 @@ namespace PositioningEngineTest
         }
 
 
-        [Test]
-        public void WhenDirectionIsEasthVersusIsForwardAndNotOnBorderNextCoordsRetunXplusOne()
-        {
-            Coords minCoord = new Coords(-10, -20);
-            Coords maxCoord = new Coords(10, 20);
-            Coords origin = new Coords(0, 0);
-            Direction direction = Direction.East;
-            Versus versus = Versus.Forward;
-            var planetGrid = new PlanetGrid(minCoord, maxCoord);
-            Coords result = planetGrid.NextCoords(origin, direction, versus);
-            result.X.Should().Be(1);
-            result.Y.Should().Be(0);
-        }
-
-        [Test]
-        public void WhenDirectionIsNorthVersusIsForwardAndNotOnBorderNextCoordsRetunYplusOne()
-        {
-            Coords minCoord = new Coords(-10, -20);
-            Coords maxCoord = new Coords(10, 20);
-            Coords origin = new Coords(0, 0);
-            Direction direction = Direction.North;
-            Versus versus = Versus.Forward;
-            var planetGrid = new PlanetGrid(minCoord, maxCoord);
-            Coords result = planetGrid.NextCoords(origin, direction, versus);
-            result.X.Should().Be(0);
-            result.Y.Should().Be(1);
-        }
-
-        [Test]
-        public void WhenDirectionIsWestVersusIsForwardAndNotOnBorderNextCoordsRetunXMinusOne()
-        {
-            Coords minCoord = new Coords(-10, -20);
-            Coords maxCoord = new Coords(10, 20);
-            Coords origin = new Coords(0, 0);
-            Direction direction = Direction.West;
-            Versus versus = Versus.Forward;
-            var planetGrid = new PlanetGrid(minCoord, maxCoord);
-            Coords result = planetGrid.NextCoords(origin, direction, versus);
-            result.X.Should().Be(-1);
-            result.Y.Should().Be(0);
-        }
-
-        [Test]
-        public void WhenDirectionIsSouthVersusIsForwardAndNotOnBorderNextCoordsRetunYMinusOne()
-        {
-            Coords minCoord = new Coords(-10, -20);
-            Coords maxCoord = new Coords(10, 20);
-            Coords origin = new Coords(0, 0);
-            Direction direction = Direction.South;
-            Versus versus = Versus.Forward;
-            var planetGrid = new PlanetGrid(minCoord, maxCoord);
-            Coords result = planetGrid.NextCoords(origin, direction, versus);
-            result.X.Should().Be(0);
-            result.Y.Should().Be(-1);
-        }
+        
 
         [Test]
         [TestCase(Versus.Forward, Direction.East, 0,0,1,0)]
@@ -104,7 +50,7 @@ namespace PositioningEngineTest
         [TestCase(Versus.Forward, Direction.West, 0, 0, -1,0)]
         [TestCase(Versus.Forward, Direction.South, 0, 0, 0, -1)]
         [Parallelizable(ParallelScope.All)]
-        public void NextCoordsIsCoeherentWIthExpectation(Versus versus, Direction direction,int startX,int startY,int expectedX,int expectedY)
+        public void NextCoordsIsCoeherentWithExpectation(Versus versus, Direction direction,int startX,int startY,int expectedX,int expectedY)
         {
             
             Coords minCoord = new Coords(-10, -20);
