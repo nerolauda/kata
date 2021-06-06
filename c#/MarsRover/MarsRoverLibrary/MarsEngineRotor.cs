03efc8e17ad792a48773276d54c035c33664ddd9
nerolauda <nerolauda@gmail.com>
2021-06-06 08:33:57 +0200

    Implementing rotation: let's do some pragmatic TDD

---
+++ c#/MarsRover/MarsRoverLibrary/MarsEngineRotor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    public class MarsEngineRotor
+    {
+        public MarsEngineDirection Direction { get; }
+
+        public MarsEngineRotor(MarsEngineDirection direction)
+        {
+            this.Direction = direction;
+        }
+
+        public MarsEngineDirection RotateLeft()
+        {
+            return MarsEngineDirection.North;
+        }
+    }
+
+    public enum MarsEngineDirection : ushort
+    {
+        East = 0,
+        North = 90,
+        West = 180,
+        South = 275
+    }
+}
---
+++ c#/MarsRover/PoistioningEngineTest/RotorTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using MarsRover;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoistioningEngineTest
+{
+    class RotorTests
+    {
+        [Test]
+        public void RotorDirectionReturnExpectedInitializedDirection()
+        {
+            foreach (MarsEngineDirection expectedDirection in Enum.GetValues(typeof(MarsEngineDirection)))
+            {
+                MarsEngineRotor rotor = new MarsEngineRotor(expectedDirection);
+                MarsEngineDirection result = rotor.Direction;
+                result.Should().Be(expectedDirection);
+            }
+        }
+
+        [Test]
+        public void WhenInitialDirectionIsEastAfterRotateLeftDirectionIsNorth()
+        {
+            var expectedDirection = MarsEngineDirection.East;
+            MarsEngineRotor rotor = new MarsEngineRotor(expectedDirection);
+            MarsEngineDirection result = rotor.RotateLeft();
+            result.Should().Be(MarsEngineDirection.North);
+        }
+
+    }
+}
