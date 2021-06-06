cde203a750162ee533667d7ab020854d5b1f96d8
nerolauda <nerolauda@gmail.com>
2021-06-06 07:54:36 +0200

    Api template and first test: move forward

---
+++ c#/MarsRover/PoistioningEngineTest/EngineTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using MarsRover;
+using NUnit.Framework;
+
+namespace PoistioningEngineTest
+{
+    class EngineTests
+    {
+        [Test]
+        public void WhenEngineMoveForwardItReturnsMinusOne()
+        {
+            int expectedValue = 1;
+
+            MarsRoverEngine engine = new MarsRoverEngine();
+            int result = engine.Move(MarsRoverEngineDirection.Forward);
+            result.Should().Be(expectedValue);
+
+        }
+    }
+}
