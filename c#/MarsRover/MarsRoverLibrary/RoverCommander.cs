d9a3c3125c3d12362b6440a61ee2bfdabc0fef01
Luca Cicale <luca.cicale@credemtel.it>
2021-06-15 10:36:14 +0200

    Commander can elaborate list of commands and break execution on obstacles

--- c#/MarsRover/MarsRoverLibrary/RoverCommander.cs
+++ c#/MarsRover/MarsRoverLibrary/RoverCommander.cs
@@ -7,22 +7,33 @@ namespace MarsRover
     public class RoverCommander
     {
         public IRover Rover { get; }
-        private Dictionary<char, Action<IRover>> commands;
+        private Dictionary<char, Func<IRover, bool>> commands;
         public RoverCommander(IRover rover)
         {
             Rover = rover;
-            commands = new Dictionary<char, Action<IRover>>();
-            commands.Add('f', rover => rover.Move(Versus.Forward));
-            commands.Add('b', rover => rover.Move(Versus.Backward));
-            commands.Add('r', rover => rover.Rotate(Rotation.Right));
-            commands.Add('l', rover => rover.Rotate(Rotation.Left));
-
-
+            commands = new Dictionary<char, Func<IRover, bool>>();
+            commands.Add('f', rover => rover.Move(Versus.Forward).ObstacleFound());
+            commands.Add('b', rover => rover.Move(Versus.Backward).ObstacleFound());
+            commands.Add('r', rover =>
+            {
+                rover.Rotate(Rotation.Right);
+                return false;
+            });
+            commands.Add('l', rover =>
+            {
+                rover.Rotate(Rotation.Left);
+                return false;
+            });
         }
 
         public void ExecuteCommands(string commandString)
         {
-            commands[commandString[0]](Rover);
+            foreach (var commandChar in commandString)
+            {
+                bool obstacleDetected = commands[commandChar](Rover);
+                if (obstacleDetected) break;
+            }
+
         }
     }
 }
--- c#/MarsRover/PositioningEngineTest/RoverCommanderTest.cs
+++ c#/MarsRover/PositioningEngineTest/RoverCommanderTest.cs
@@ -44,6 +44,40 @@ namespace MarsRoverTest
             RoverCommander commander = new RoverCommander(roverMock.Object);
             commander.ExecuteCommands("l");
         }
+        [Test]
+        public void WhenReceiveMoreCommandsAndMoveDoNotDetectObstaclesCommandRoverInvokesAllOfThemInSequence()
+        {
+            var roverMock = new Mock<IRover>(MockBehavior.Strict);
+            var sequence = new MockSequence();
+            roverMock.InSequence(sequence).Setup(rover => rover.Rotate(Rotation.Left));
+            roverMock.InSequence(sequence).Setup(rover => rover.Move(Versus.Forward)).Returns(new MoveResult());
+            roverMock.InSequence(sequence).Setup(rover => rover.Rotate(Rotation.Right));
+            roverMock.InSequence(sequence).Setup(rover => rover.Move(Versus.Backward)).Returns(new MoveResult());
+            RoverCommander commander = new RoverCommander(roverMock.Object);
+            commander.ExecuteCommands("lfrb");
+            roverMock.Verify(rover => rover.Rotate(Rotation.Left), Times.Once);
+            roverMock.Verify(rover => rover.Move(Versus.Forward), Times.Once);
+            roverMock.Verify(rover => rover.Rotate(Rotation.Right), Times.Once);
+            roverMock.Verify(rover => rover.Move(Versus.Backward), Times.Once);
+
+        }
+
+        [Test]
+        public void WhenReceiveMoreCommandsAndMoveDetectObstacleCommandRoverInvokesAllOfThemInSequenceAndStopBeforeObstacle()
+        {
+            var roverMock = new Mock<IRover>(MockBehavior.Strict);
+            MoveResult resultWithObstacle = new MoveResult();
+            resultWithObstacle.ObstacledAt(new Coords(10, 0));
+
+            roverMock.Setup(rover => rover.Rotate(Rotation.Right));
+            roverMock.Setup(rover => rover.Move(Versus.Forward)).Returns(resultWithObstacle);
+            RoverCommander commander = new RoverCommander(roverMock.Object);
+            commander.ExecuteCommands("rfbbrbfllf");
+            roverMock.Verify(rover => rover.Rotate(Rotation.Right), Times.Once);
+            roverMock.Verify(rover => rover.Move(Versus.Forward), Times.Once);
+            roverMock.Verify(rover => rover.Rotate(Rotation.Left), Times.Never);
+            roverMock.Verify(rover => rover.Move(Versus.Backward), Times.Never);
+        }
 
     }
 }
